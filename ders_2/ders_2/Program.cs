e90c7084e3f26d5b1e882cb8fd8200ba20acd818
Işıl Zülal Güner <72742054+Kiarikita@users.noreply.github.com>
2021-11-04 19:56:16 +0300

    Add files via upload

---
+++ ders_2/ders_2/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("Lütfen adınızı yazınız:");
+            string name = Console.ReadLine();
+            Console.Write("Lütfen soyadınızı yazınız:");
+            string surname = Console.ReadLine();
+
+            string first_letter_of_name = name.Substring(0, 1).ToUpper();
+            string first_letter_of_surname = surname.Substring(0, 1).ToUpper();
+
+            string newName = first_letter_of_name + name.Substring(1).ToLower();
+            string newSurname = first_letter_of_surname + surname.Substring(1).ToLower();
+
+            string fullName = newName + " " +newSurname;
+            
+            Console.WriteLine(fullName);
+
+            Console.ReadLine();
+            */
+
+            /*string sentence = "Arabam 70 model";
+
+            int number7 = sentence.IndexOf("7");
+
+            string result = sentence.Substring(number7);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            */
+
+            /*int sayi1 = 1;
+            int sayi2 = 3;
+            int toplam = sayi1 + sayi2;
+
+            Console.WriteLine(
+                "1.Sayı = " + sayi1 +
+                " 2.Sayı = " + sayi2+
+                " Toplam = " + toplam);
+
+            Console.ReadLine();
+            */
+
+            //console'a yazılan ve okunan her şey string!!
+
+            /*double pi = 3.14;
+            float sayi = 3.14f;
+            decimal sayi2 = 3.14M;
+
+            Console.WriteLine(pi);
+            Console.ReadLine();
+            */
+
+            /*bool value1 = true;
+            bool value2 = false;
+            bool value3 = 10 > 15;
+
+            Console.WriteLine(value3);
+            Console.ReadLine();
+            */
+
+
+            //DateTime dt = new DateTime(2021, 11, 1, 21, 5, 50);
+
+            /*DateTime dt = DateTime.Now;
+            DateTime dt2 = DateTime.Now;
+            //veritabanına işlem saati kaydedilirken kullanılacak
+            dt = dt.AddDays(5);
+            double fark = (dt2 - dt).TotalSeconds;
+
+            Console.WriteLine(dt);
+            Console.WriteLine();
+            Console.ReadLine();
+            */
+
+            /*
+            Object
+            object str_var = "zülal";
+            object int_var = 5;
+            object dbl_var = 3.14;
+            object dt_var = DateTime.Now;
+            object bool_var = false;
+
+
+            Console.WriteLine(
+                "string value = "+ str_var + "\n" +
+                "int value = " + int_var + "\n" +
+                "double value = " + dbl_var + "\n" +
+                "datetime value = " + dt_var + "\n" +
+                "boolean value = " + bool_var );
+            Console.ReadLine();
+
+            //boxing and Unboxing
+
+            object int_var = 5;
+            int sayi = 8;
+
+            int_var = sayi; //boxing
+
+            sayi = (int)int_var; //unboxing --> sayi = int_var; --> bu hatalı - sayıyı kutuya eşitlemek istiyor gibi algılar
+            */
+
+            /*int[] sayilar = new int[3]; // 3 item sayısı
+
+            sayilar[0] = 40;
+            sayilar[1] = 30;
+            sayilar[2] = 20;
+            */
+            /*
+            int[] sayilar = {40, 30 ,20};
+
+            sayilar[1] = 785;  //dinamik yapı
+            Console.WriteLine(
+                "array 0. index = " + sayilar[0] + "\n" +
+                "array 1. index = " + sayilar[1] + "\n" +
+                "array 2. index = " + sayilar[2] + "\n");
+            Console.WriteLine(sayilar.Length);
+            Console.ReadLine();
+            */
+
+            string[] names = { "Sümeyra", "Zülal", "Işıl" };
+
+            names[1] = "Özlem";  //dinamik yapı
+            Console.WriteLine(
+                "array 0. index = " + names[0] + "\n" +
+                "array 1. index = " + names[1] + "\n" +
+                "array 2. index = " + names[2] + "\n");
+            Console.WriteLine(names.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ders_3/ders_3/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Array double örneği
+            /*double[] sayilar = { 3.14, 5.6, 7.3};
+
+            Console.WriteLine(
+                "Array 0. index= " + sayilar[0] + "\n" +
+                "Array 1. index= " + sayilar[1] + "\n" +
+                "Array 2. index= " + sayilar[2]);
+            Console.ReadLine();
+            */
+            /*
+             * List mantığı
+            List<string> isimler = new List<string>();
+
+            isimler.Add("ali");
+            isimler.Add("veli");
+            isimler.Add("yiğit");
+            //isimler.AddRange(new List<string> { "mert", "canan"});
+
+            foreach (var item in isimler) 
+                Console.WriteLine(item);
+
+
+            //isimler.Remove("ali"); 
+            //isimler.RemoveAt(1); //şu indexteki itemı sil 
+            //isimler.RemoveRange(0,2);//0.itemden başla 2 tanesini sil
+
+            
+
+            //isimler.Clear(); //açık sarılar method parantez aç ve kapa parametre gerekir
+            //clear ile listeyi tamamen boşalttık
+
+            isimler.Insert(1, "Yeni Kişi"); // add en sona ekler, insert için index verirsiniz araya ekler -verilen index sayısının yerine-
+            Console.WriteLine(isimler.Count());
+
+            Console.WriteLine("*************************");
+            foreach (var item in isimler)
+                Console.WriteLine(item);
+            */
+
+            /*Console.WriteLine("Lütfen yaşınızı giriniz= ");
+            int age = int.Parse(Console.ReadLine()); //convertten daha hızlı ama convert daha yetenekli
+
+            Console.WriteLine("Yaşınız: " + age);
+            */
+            /*
+            string age = "25";
+            int int_age = Convert.ToInt32(age);
+
+            bool dogru = true;
+            //int bool_dogru = int.Parse(dogru); //convert edemiyor, mutlaka string ifade görmek ister
+            int bool_dogru = Convert.ToInt32(dogru); //convert daha masraflı 
+            Console.WriteLine(dogru);
+            */
+
+            /*
+            string date = "15.05.1999 21:40";
+            DateTime my_birth = Convert.ToDateTime(date);
+
+            Console.WriteLine(my_birth);
+            Console.ReadLine();
+            */
+
+            //karar mekanizmaları
+
+            /*Console.WriteLine("Lütfen bir sayı giriniz: ");
+
+            int sayi = int.Parse(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir.");
+            }
+            else 
+            {
+                Console.WriteLine("Sayı tektir.");
+            }
+            */
+
+            /*Console.WriteLine("Lütfen notunuzu giriniz: ");
+
+            int note = int.Parse(Console.ReadLine());
+
+            if (note < 40)
+            {
+                Console.WriteLine("Kaldınız...");
+            }
+            else if(note < 50)
+            {
+                Console.WriteLine("Geçtiniz. 2 aldınız.");
+            }
+            else if (note < 70)
+            {
+                Console.WriteLine("Geçtiniz. 3 aldınız.");
+            }
+            else if (note < 85)
+            {
+                Console.WriteLine("Geçtiniz. 4 aldınız.");
+            }
+            else if (note < 101)
+            {
+
+                if (95< note)
+                {
+                    Console.WriteLine("Tebrikler.");
+                }
+                Console.WriteLine("Geçtiniz. 5 aldınız.");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz not girişi.");
+            }
+            */
+
+            /*Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen bir isim giriniz: ");
+            string isim = Console.ReadLine();
+
+            if (isim == "murat" || sayi <5 || sayi %2 == 0)  // ve -->&& *** veya -->|| *** 
+            {
+                Console.WriteLine("İsminiz murat ve/veya sayı 5 ten küçüktür.");
+            }
+            else
+            {
+                Console.WriteLine("değildir.");
+            }
+            */
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            if (sayi + sayi2 < 100)
+            {
+                Console.WriteLine("toplam 100 den küçük");
+            }
+            else
+            {
+                Console.WriteLine("toplam 100 den büyük");
+            }
+            Console.ReadLine();
+        }
+    }
+}
