1477b8a3e5c1810588d4ce62fac5cb0be19f3a23
Işıl Zülal Güner <72742054+Kiarikita@users.noreply.github.com>
2021-12-03 22:00:14 +0300

    Add files via upload

---
+++ ders_16_login_project/ders_16_login_project/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders_16_login_project
+{
+    public partial class frmLogin : Form
+    {
+        UserManager userManager;
+
+        public frmLogin()
+        {
+            InitializeComponent();//label textbox vs. ekler çalıştırır
+            userManager = UserManager.GetInstance();//ban abir tane obje yarat dedik, o esnada null olup olmadığına bakılır
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text!= "" && txtPassword.Text!="")
+            {
+                //yeni form açarız
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adınız ve ya şifreniz hatalı...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
---
+++ ders_16_login_project/ders_16_login_project/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_16_login_project
+{
+    public class User
+    {
+        //user bizim için bir entity
+        int _id;
+        string _name, _password, _question, _answer;
+
+        public User(int id, string name, string password, string question, string answer)
+        {
+            _id = id;
+            _name = name;
+            _password = password;
+            _question = question;
+            _answer = answer;
+            
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Question { get => _question; set => _question = value; }
+        public string Answer { get => _answer; set => _answer = value; }
+    }
+}
